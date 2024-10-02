9b4ae305225b4b8b3a3b49a9627bf37ad883e378
Thiago Alexandre <thiagoasilvapro@gmail.com>
2024-10-02 17:10:21 -0300

    Novo commit

--- ProConsulta/obj/Debug/net8.0/ProConsulta.AssemblyInfo.cs
+++ ProConsulta/obj/Debug/net8.0/ProConsulta.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProConsulta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+211270c96f44338d1a032f5bcb705ba6621ac943")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bae100bfc8fbcf8bbc2e65a92ed907c79005f09")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProConsulta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProConsulta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
